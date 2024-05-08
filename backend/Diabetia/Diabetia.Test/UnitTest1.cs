e6a5d0f14379ea56d9b6c56c7264fffb32398fca
NROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2024-05-08 19:19:44 -0300

    add tests to backend

---
+++ backend/Diabetia/Diabetia.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Diabetia.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
