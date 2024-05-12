73bd74b8678c01a84f2d59b32f207108562f29d9
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2024-05-12 17:49:47 -0300

    add cookies login logic

--- backend/Diabetia/Diabetia.API/Program.cs
+++ backend/Diabetia/Diabetia.API/Program.cs
@@ -1,13 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(options =>
+{
+    options.WithOrigins("http://localhost:3000")
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+           .AllowCredentials();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
