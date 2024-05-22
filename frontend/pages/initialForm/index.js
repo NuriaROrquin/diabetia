7900d32a6a90fcf0a76fe8859fbc4f3af8bc59c4
vgambaro <victoriagambaro@gmail.com>
2024-05-22 12:56:05 -0300

    Step 1 del Formulario inicial

--- frontend/pages/initialForm/index.js
+++ frontend/pages/initialForm/index.js
@@ -1,6 +1,11 @@
+import {BlueLink, OrangeLink} from "../../components/link";
+
 const InitialForm = () => {
     return(
-        <h1>Initial Form</h1>
+        <>
+        <h1>Bienvenido, por única vez te haremos unas preguntas para iniciar con la aplicación</h1>
+        <OrangeLink href="initialForm/step-1"/>
+        </>
     )
 }
 
