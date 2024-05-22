5ba402bac4c90834c9f7f3f4a333217d4a60a47a
vgambaro <victoriagambaro@gmail.com>
2024-05-22 17:27:07 -0300

    arreglos en el initial form

--- frontend/pages/initialForm/index.js
+++ frontend/pages/initialForm/index.js
@@ -12,14 +12,17 @@ const InitialForm = () => {
     return(
         <Section className="pt-12">
             <div className="container items-center flex w-full justify-center flex-col">
-                <TitleSection className="text-white">Bienvenido a DiabetIA!</TitleSection>
+                <TitleSection className="text-white">¡Bienvenido a DiabetIA!</TitleSection>
+                <div className="bg-white rounded-xl w-full flex flex-wrap text-gray-primary py-20 px-44 my-12 justify-around gap-x-2 gap-y-12">
 
-                <p className="text-gray-primary">Por única vez, vamos a solicitarte algunos datos para poder ser más
-                    precisos con la información que te vamos a brindar</p>
+                    <p className="text-gray-primary">Por única vez, vamos a solicitarte algunos datos para poder ser más
+                        precisos con la información que te vamos a brindar</p>
+                    <OrangeLink href="/initialForm/step-1" label="¡Estoy listo!" width="w-1/10"/>
 
+                </div>
             </div>
         </Section>
-    )
+)
 }
 
 export default InitialForm;
--- frontend/pages/initialForm/step-2/index.js
+++ frontend/pages/initialForm/step-2/index.js
@@ -10,6 +10,7 @@ import dayjs from "dayjs";
 import {ButtonOrange} from "../../../components/button";
 import {CustomDatePicker, CustomTimePicker} from "../../../components/pickers";
 import {useCookies} from "react-cookie";
+import {Step, StepLabel, Stepper} from "@mui/material";
 
 const InitialFormStep2 = () => {
     const [isOpenTipoDiabetes, setIsOpenTipoDiabetes] = useState(false);
@@ -39,15 +40,23 @@ const InitialFormStep2 = () => {
         setIsOpenFrecuenciaInsulina(false);
     };
 
+    const steps = [
+        'Datos personales',
+        'Datos del paciente',
+        'Actividad física y salud',
+        'Dispositivos y sensores',
+    ];
+
 
     const handleSubmit = () => {
         const tipoDiabetes = selectedOptionTipoDiabetes;
         const usaInsulina = insuline;
         const tipoInsulina = selectedOptionTipoInsulina;
+        const FrecuenciaInsulina = selectedOptionFrecuenciaInsulina;
         const quiereReminder = reminder;
         const dateFormatted = date ? date.format('DD-MM-YYYY') : null;
 
-        console.log("Datos del formulario:", tipoDiabetes, usaInsulina, tipoInsulina, FrecuenciaInsulina, weight, email, phone);
+        console.log("Datos del formulario:", tipoDiabetes, usaInsulina, tipoInsulina, FrecuenciaInsulina, quiereReminder, dateFormatted);
         setCookie("informationCompleted", true, {path: "/", expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)});
         window.location.href = '/';
     }
@@ -59,15 +68,38 @@ const InitialFormStep2 = () => {
 
 
                 {/* FORMULARIO */}
-                <div className="bg-white rounded-xl w-full flex flex-wrap text-gray-primary py-20 px-44 my-12 justify-around gap-x-2 gap-y-12">
+                <div
+                    className="bg-white rounded-xl w-full flex flex-wrap text-gray-primary py-20 px-44 my-12 justify-around gap-x-2 gap-y-12">
+                    <div className="flex flex-col w-full gap-12">
+                        <Stepper activeStep={1} alternativeLabel>
+                            {steps.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                        <TitleSection className="w-full !text-blue-secondary">Datos Personales</TitleSection>
 
-                    <Select label="Tipo de Diabetes" placeholder="¿Qué tipo de diabetes tenés?" options={TYPE_DIABETES} selectedOption={selectedOptionTipoDiabetes} handleOptionClick={handleOptionClickTipoDiabetes} setIsOpen={setIsOpenTipoDiabetes} isOpen={isOpenTipoDiabetes} width="w-1/3"/>
-                    <CustomSwitch label="¿Usás insulina?" id="insulin" onChange={() => setInsuline(!insuline)} width="w-1/3"/>
+                    </div>
+                    <Select label="Tipo de Diabetes" placeholder="¿Qué tipo de diabetes tenés?" options={TYPE_DIABETES}
+                            selectedOption={selectedOptionTipoDiabetes}
+                            handleOptionClick={handleOptionClickTipoDiabetes} setIsOpen={setIsOpenTipoDiabetes}
+                            isOpen={isOpenTipoDiabetes} width="w-1/3"/>
+                    <CustomSwitch label="¿Usás insulina?" id="insulin" onChange={() => setInsuline(!insuline)}
+                                  width="w-1/3"/>
                     {insuline && (
                         <>
-                        <Select label="Tipo de Insulina" placeholder="Indicá tipo de insulina" options={TYPE_INSULIN} selectedOption={selectedOptionTipoInsulina} handleOptionClick={handleOptionClickTipoInsulina} setIsOpen={setIsOpenTipoInsulina} isOpen={isOpenTipoInsulina} width="w-1/3"/>
-                        <Select label="Frecuencia de inyecciones" placeholder="Indicá tipo de insulina" options={INSULIN_FREQUENCY} selectedOption={selectedOptionFrecuenciaInsulina} handleOptionClick={handleOptionClickFrecuenciaInsulina} setIsOpen={setIsOpenFrecuenciaInsulina} isOpen={isOpenFrecuenciaInsulina} width="w-1/3"/>
-                        <CustomSwitch label="¿Querés un recordatorio de aplicación?" id="reminder" onChange={() => setReminder(!reminder)} width="w-1/3"/>
+                            <Select label="Tipo de Insulina" placeholder="Indicá tipo de insulina"
+                                    options={TYPE_INSULIN} selectedOption={selectedOptionTipoInsulina}
+                                    handleOptionClick={handleOptionClickTipoInsulina} setIsOpen={setIsOpenTipoInsulina}
+                                    isOpen={isOpenTipoInsulina} width="w-1/3"/>
+                            <Select label="Frecuencia de inyecciones" placeholder="Indicá tipo de insulina"
+                                    options={INSULIN_FREQUENCY} selectedOption={selectedOptionFrecuenciaInsulina}
+                                    handleOptionClick={handleOptionClickFrecuenciaInsulina}
+                                    setIsOpen={setIsOpenFrecuenciaInsulina} isOpen={isOpenFrecuenciaInsulina}
+                                    width="w-1/3"/>
+                            <CustomSwitch label="¿Querés un recordatorio de aplicación?" id="reminder"
+                                          onChange={() => setReminder(!reminder)} width="w-1/3"/>
                             <CustomTimePicker
                                 label="Hora del recordatorio"
                                 value={Hour}
@@ -77,7 +109,8 @@ const InitialFormStep2 = () => {
                                 className={reminder ? '' : 'hidden'}
                             />
                         </>
-                        )}
+                    )}
+                    <OrangeLink href="/initialForm/step-1" label="Atrás" width="w-1/3"/>
                     <ButtonOrange onClick={handleSubmit} label="Finalizar" width="w-1/3"/>
                 </div>
             </div>
