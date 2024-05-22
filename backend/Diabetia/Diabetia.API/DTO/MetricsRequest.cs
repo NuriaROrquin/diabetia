d49abfe3f6cc304ed771c5a4ccb4c224df63302b
Facundo Damián Fagnano <facundofagnano@gmail.com>
2024-05-22 16:59:09 -0300

    Commit nuevo

--- backend/Diabetia/Diabetia.API/DTO/MetricsRequest.cs
+++ backend/Diabetia/Diabetia.API/DTO/MetricsRequest.cs
@@ -2,11 +2,7 @@
 {
     public class MetricsRequest
     {
-        public string? Email { get; set; }
-
-        public int IdUser { get; set; }
-
-        public int IdEvento { get; set; }
+        public string Email { get; set; }
 
     }
 
