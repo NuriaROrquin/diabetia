2fe491e571780933197e8783c654d70cb23e2e94
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2024-05-22 14:00:33 -0300

    DIAB-1-2 stepper

--- frontend/pages/initialForm/step-1/index.js
+++ frontend/pages/initialForm/step-1/index.js
@@ -9,17 +9,13 @@ import {Select} from "../../../components/selector";
 import dayjs from "dayjs";
 import {ButtonOrange} from "../../../components/button";
 import {CustomDatePicker, CustomTimePicker} from "../../../components/pickers";
+import {useRouter} from "next/router";
+import {Step, StepLabel, Stepper} from "@mui/material";
 
 const InitialFormStep1 = () => {
     const eventSelected = TYPE_EVENTS.filter((event) => event.id === 2)[0].title;
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedOption, setSelectedOption] = useState(null);
     const [date, setDate] = useState()
-
-    const handleOptionClick = (option) => {
-        setSelectedOption(option);
-        setIsOpen(false);
-    };
+    const router = useRouter()
 
     const handleSubmit = () => {
         const name = document.getElementById("name").value;
@@ -30,30 +26,36 @@ const InitialFormStep1 = () => {
         const phone = document.getElementById("phone").value;
 
         console.log("Datos del formulario:", name, surname, dateFormatted, weight, email, phone);
+        router.push("/initialForm/step-2")
     }
+
+    const steps = [
+        'Datos personales',
+        'Datos del paciente',
+        'Actividad física y salud',
+        'Dispositivos y sensores',
+    ];
 
     return(
         <Section className="pt-12">
             <div className="container items-center flex w-full justify-center flex-col">
-                <TitleSection className="text-white">¿Qué evento querés cargar?</TitleSection>
-                <div className="flex w-full flex-wrap gap-y-6 gap-x-24 justify-center mt-8">
-                    {TYPE_EVENTS.map((event) => {
-                        return(
-                            <>
-                                {event.title === eventSelected ?
-                                    <OrangeLink key={event.title} label={capitalizeFirstLetter(event.title)} width="w-1/6" href={event.link} />
-                                    :
-                                    <BlueLink key={event.title} label={capitalizeFirstLetter(event.title)} width="w-1/6" href={event.link} />
-                                }
-                            </>
-                        )
-                    })}
-                </div>
-
                 {/* FORMULARIO */}
-                <div className="bg-white rounded-xl w-full flex flex-wrap text-gray-primary py-20 px-44 my-12 justify-around gap-x-2 gap-y-12">
-                    <InputWithLabel label="Nombre" placeholder="Juan"  id="name" width="w-1/3"/>
-                    <InputWithLabel label="Apellido" placeholder="Perez"  id="surname" width="w-1/3"/>
+                <div
+                    className="bg-white rounded-xl w-full flex flex-wrap text-gray-primary py-20 px-44 my-12 justify-around gap-x-2 gap-y-12">
+                    <div className="flex flex-col w-full gap-12">
+                        <Stepper activeStep={0} alternativeLabel>
+                            {steps.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                        <TitleSection className="w-full !text-blue-secondary">Datos Personales</TitleSection>
+
+                    </div>
+
+                    <InputWithLabel label="Nombre" placeholder="Juan" id="name" width="w-1/3"/>
+                    <InputWithLabel label="Apellido" placeholder="Perez" id="surname" width="w-1/3"/>
                     <CustomDatePicker
                         label="Fecha de nacimiento"
                         value={date}
@@ -61,9 +63,10 @@ const InitialFormStep1 = () => {
                         defaultValue={dayjs()}
                         width="w-1/3"
                     />
-                    <InputWithLabel label="Peso" placeholder="Ingresá tu peso"  id="weight" width="w-1/3"/>
-                    <InputWithLabel label="Correo Electrónico" placeholder="email@diabetia.com.ar"  id="email" width="w-1/3"/>
-                    <InputWithLabel label="Teléfono" placeholder="1234-5678"  id="phone" width="w-1/3"/>
+                    <InputWithLabel label="Peso" placeholder="Ingresá tu peso" id="weight" width="w-1/3"/>
+                    <InputWithLabel label="Correo Electrónico" placeholder="email@diabetia.com.ar" id="email"
+                                    width="w-1/3"/>
+                    <InputWithLabel label="Teléfono" placeholder="1234-5678" id="phone" width="w-1/3"/>
 
                     <ButtonOrange onClick={handleSubmit} label="Siguiente" width="w-1/3"/>
                 </div>
