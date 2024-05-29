c5762527d5074d4ae352d053e6906a226b0920f1
Pablo Antunez <pablooantunez@hotmail.com>
2024-05-28 22:36:38 -0300

    Add Testing and Exceptions

--- backend/Diabetia/Diabetia.Infrastructure/Middlewares/ErrorHandlerMiddleware.cs
+++ backend/Diabetia/Diabetia.Infrastructure/Middlewares/ErrorHandlerMiddleware.cs
@@ -1,10 +1,7 @@
-﻿using Amazon.CognitoIdentity.Model;
-using Amazon.CognitoIdentityProvider.Model;
-using Amazon.Runtime.Internal;
+﻿using Amazon.CognitoIdentityProvider.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Net;
-using System.Net.Http;
 using System.Text.Json;
 
 namespace Diabetia.Infrastructure.Middlewares
@@ -19,26 +16,11 @@ namespace Diabetia.Infrastructure.Middlewares
             _next = next;
             _logger = logger;
         }
-
         public async Task Invoke(HttpContext context)
         {
             try
             {
                 await _next(context);
-            }
-            catch (UsernameExistsException ex)
-            {
-                _logger.LogError(ex, "An unhandled exception occurred.");
-                context.Response.ContentType = "application/json";
-                context.Response.StatusCode = 409;
-
-                var errorResponse = JsonSerializer.Serialize(new
-                {
-                    StatusCode = ex.StatusCode,
-                    Message = "El usuario ya está registrado"
-                });
-
-                await context.Response.WriteAsync(errorResponse);
             }
             catch (Exception ex)
             {
@@ -47,16 +29,39 @@ namespace Diabetia.Infrastructure.Middlewares
             }
         }
 
-        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
         {
             context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-            return context.Response.WriteAsync(new
+            if (ex is UsernameExistsException)
+            {
+                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.Conflict, "El usuario ya está registrado");
+            }
+            else if (ex is InvalidPasswordException)
+            {
+                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Contraseña inválida");
+            }
+            else if (ex is InvalidParameterException)
+            {
+                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Parámetros de solicitud inválidos");
+            }
+            else
+            {
+                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.InternalServerError, "Este es un mensaje de error custom");
+            }
+        }
+
+        private async Task HandleExceptionWithStatusCode(HttpContext context, Exception ex, HttpStatusCode statusCode, string message)
+        {
+            context.Response.StatusCode = (int)statusCode;
+
+            var errorResponse = JsonSerializer.Serialize(new
             {
                 StatusCode = context.Response.StatusCode,
-                Message = "Este es un mensaje de error custom"
-            }.ToString());
+                Message = message
+            });
+
+            await context.Response.WriteAsync(errorResponse);
         }
-    }
+    } 
 }
