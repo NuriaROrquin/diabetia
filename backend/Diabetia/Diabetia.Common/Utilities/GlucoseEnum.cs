b68e0db53698f78514210eb1c0561f14aa5c780f
NROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2024-06-01 01:48:36 -0300

    Create GlucoseEnum.cs

---
+++ backend/Diabetia/Diabetia.Common/Utilities/GlucoseEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diabetia.Common.Utilities
+{
+    public enum GlucoseEnum
+    {
+        HIPOGLUCEMIA = 1,
+        HIPERGLUCEMIA = 2
+    }
+}
