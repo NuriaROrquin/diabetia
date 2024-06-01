976e2138c7589d572a5f50c37c9cd5616c2f4c61
NROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2024-05-31 22:27:05 -0300

    Update logout.js

--- frontend/pages/api/logout.js
+++ frontend/pages/api/logout.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
     res.setHeader('Set-Cookie', 'jwt=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=Strict');
+    res.setHeader('Set-Cookie', 'informationCompleted=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=Strict');
     res.status(200).json({ message: 'Logged out' });
 }
