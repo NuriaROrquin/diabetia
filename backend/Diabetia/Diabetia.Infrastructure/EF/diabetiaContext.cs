0ff469747f9fc97ed942fe36b8def6e84b16d034
NROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2024-06-11 18:41:47 -0300

    New scaffold

--- backend/Diabetia/Diabetia.Infrastructure/EF/diabetiaContext.cs
+++ backend/Diabetia/Diabetia.Infrastructure/EF/diabetiaContext.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Diabetia.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Diabetia.Infrastructure.EF
 {
@@ -824,12 +824,6 @@ namespace Diabetia.Infrastructure.EF
                 entity.Property(e => e.IdActividadFisica).HasColumnName("id_actividad_fisica");
 
                 entity.Property(e => e.IdPaciente).HasColumnName("id_paciente");
-
-                entity.HasOne(d => d.IdActividadFisicaNavigation)
-                    .WithMany(p => p.PacienteActividadFisicas)
-                    .HasForeignKey(d => d.IdActividadFisica)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("paciente_actividad_fisica_ibfk_2");
 
                 entity.HasOne(d => d.IdPacienteNavigation)
                     .WithMany(p => p.PacienteActividadFisicas)
