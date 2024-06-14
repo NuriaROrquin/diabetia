f7a2b2d90fdda78f75e5392cae712a1e058b5d63
Pablo Antunez <pablooantunez@hotmail.com>
2024-06-14 20:08:11 -0300

    Fix Middleware, add dictionary instead of using elif

--- backend/Diabetia/Diabetia.Infrastructure/Middlewares/ErrorHandlerMiddleware.cs
+++ backend/Diabetia/Diabetia.Infrastructure/Middlewares/ErrorHandlerMiddleware.cs
@@ -34,176 +34,63 @@ namespace Diabetia.Infrastructure.Middlewares
         private async Task HandleExceptionAsync(HttpContext context, Exception ex)
         {
             context.Response.ContentType = "application/json";
-
-            // Register Exceptions
-            if (ex is UsernameExistsException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.Conflict, "El usuario ya está registrado");
-            }
-            else if (ex is NotAuthorizedException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Usuario o contraseña incorrectos");
-            }
-            else if (ex is InvalidPasswordException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Contraseña inválida");
-            }
-            else if (ex is InvalidParameterException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Parámetros de solicitud inválidos");
-            }
-            else if (ex is ExpiredCodeException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El código ingresado ha expirado");
-            }
-            else if (ex is UserNotFoundException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.NotFound, "Usuario no encontrado");
-            }
-            else if (ex is CodeMismatchException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El código ingresado es incorrecto");
-            }
-            else if (ex is InvalidEmailException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El email no tiene un formato válido");
-            }
-            else if (ex is UsernameNotFoundException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Nombre de usuario no encontrado");
-            }
-            else if (ex is InvalidOperationException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Operación inválida");
-            }
-            else if (ex is EmailAlreadyExistsException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El email ya se encuentra registrado");
-            }
-
-            // Change Password Exceptions
-            else if (ex is UserNotConfirmedException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El usuario aún no se encuentra confirmado");
-            }
-
-            // Forgot Password Exceptions
-            else if (ex is UserNotAuthorizedException) // Tambien del Login
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El usuario no se encuentra autenticado");
-            }
-            else if (ex is TooManyRequestsException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "La contraseña ingresada no es válida");
-            }
-            else if (ex is LimitExceededException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "La contraseña ingresada no es válida");
-            }
-
-            // Confirm Forgot Password Exceptions
-            else if (ex is TooManyFailedAttemptsException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Alcanzó el número máximo de intentos");
-            }
-            // Login Exceptions
-            else if (ex is PasswordResetRequiredException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "La contraseña ingresada caducó, necesita renovarla");
-            }
-            else if (ex is NoInformationUserException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Información del usuario no encontrada");
-            }
-            else if (ex is Amazon.CognitoIdentityProvider.Model.NotAuthorizedException) // Asegúrate de que esta línea está correcta
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Usuario o contraseña incorrectos");
-            }
-            else if (ex is Amazon.Runtime.Internal.HttpErrorResponseException httpEx &&
-             httpEx.InnerException is Amazon.CognitoIdentityProvider.Model.NotAuthorizedException cognitoEx)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Usuario o contraseña incorrectos");
-            }
-
-            // Event Exceptions
-            else if (ex is EventNotFoundException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Evento no encontrado");
-            }
-            else if (ex is UserEventNotFoundException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El usuario no tiene asociado el evento seleccionado");
-            }
-            else if (ex is PatientNotFoundException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El usuario todavía no fue dado de alta como paciente");
-            }
-            else if (ex is EventNotRelatedWithPatientException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El evento no se encuentra relacionado al paciente");
-            }
-            else if (ex is PhysicalEventNotMatchException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "La actividad física seleccionada es erronea");
-            }
-            else if (ex is MismatchUserPatientException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El usuario no posee el evento asociado");
-            }
-            else if (ex is UserNotFoundOnDBException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El usuario no se encuentra registrado");
-
-            }
-            else if (ex is PatientInsulinRelationNotFoundException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El paciente no se encuentra relacionado a esta insulina");
-            }
-            else if (ex is GlucoseEventNotMatchException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "La carga de glucosa seleccionada es errónea.");
-            }
-            else if (ex is InsulinEventNotMatchException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "La carga de insulina seleecionada es errónea.");
-            }
-            else if (ex is FoodEventNotMatchException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "El registro de comida seleecionado es errónea.");
-            }
-            else if (ex is IngredientFoodRelationNotFoundException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Este ingrediente no se encuentra relacionado con ningún evento de comida.");
-            }
-            else if (ex is CantCreatObjectS3Async)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "No se pudo pudo guardar su archivo PDF correctamente.");
-            }
-            else if (ex is ExaminationEventNotFoundException)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "Este evento no se encuentra relacionado con ningún estudio médico.");
-            }
-            else if (ex is CantDeleteObjectS3Async)
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.BadRequest, "No se pudo pudo eliminar su archivo PDF correctamente.");
-            }
-            else
-            {
-                await HandleExceptionWithStatusCode(context, ex, HttpStatusCode.InternalServerError, "Este es un mensaje de error custom");
-            }
-        }
-
-        private async Task HandleExceptionWithStatusCode(HttpContext context, Exception ex, HttpStatusCode statusCode, string message)
-        {
-            context.Response.StatusCode = (int)statusCode;
-
+            var exceptionDetails = GetExceptionDetails(ex);
+            context.Response.StatusCode = (int)exceptionDetails.StatusCode;
             var errorResponse = JsonSerializer.Serialize(new
             {
                 StatusCode = context.Response.StatusCode,
-                Message = message
+                Message = exceptionDetails.Message
             });
-
             await context.Response.WriteAsync(errorResponse);
         }
+
+        private (HttpStatusCode StatusCode, string Message) GetExceptionDetails(Exception ex)
+        {
+            var exceptionMap = new Dictionary<Type, (HttpStatusCode StatusCode, string Message)>
+        {
+            { typeof(UsernameExistsException), (HttpStatusCode.Conflict, "El usuario ya está registrado") }, // key = nameException ; value = (statusCode + message) -> tuple
+            { typeof(NotAuthorizedException), (HttpStatusCode.BadRequest, "Usuario o contraseña incorrectos") },
+            { typeof(InvalidPasswordException), (HttpStatusCode.BadRequest, "Contraseña inválida") },
+            { typeof(InvalidParameterException), (HttpStatusCode.BadRequest, "Parámetros de solicitud inválidos") },
+            { typeof(ExpiredCodeException), (HttpStatusCode.BadRequest, "El código ingresado ha expirado") },
+            { typeof(UserNotFoundException), (HttpStatusCode.NotFound, "Usuario no encontrado") },
+            { typeof(CodeMismatchException), (HttpStatusCode.BadRequest, "El código ingresado es incorrecto") },
+            { typeof(InvalidEmailException), (HttpStatusCode.BadRequest, "El email no tiene un formato válido") },
+            { typeof(UsernameNotFoundException), (HttpStatusCode.BadRequest, "Nombre de usuario no encontrado") },
+            { typeof(InvalidOperationException), (HttpStatusCode.BadRequest, "Operación inválida") },
+            { typeof(EmailAlreadyExistsException), (HttpStatusCode.BadRequest, "El email ya se encuentra registrado") },
+            { typeof(UserNotConfirmedException), (HttpStatusCode.BadRequest, "El usuario aún no se encuentra confirmado") },
+            { typeof(UserNotAuthorizedException), (HttpStatusCode.BadRequest, "El usuario no se encuentra autenticado") },
+            { typeof(TooManyRequestsException), (HttpStatusCode.BadRequest, "La contraseña ingresada no es válida") },
+            { typeof(LimitExceededException), (HttpStatusCode.BadRequest, "La contraseña ingresada no es válida") },
+            { typeof(TooManyFailedAttemptsException), (HttpStatusCode.BadRequest, "Alcanzó el número máximo de intentos") },
+            { typeof(PasswordResetRequiredException), (HttpStatusCode.BadRequest, "La contraseña ingresada caducó, necesita renovarla") },
+            { typeof(NoInformationUserException), (HttpStatusCode.BadRequest, "Información del usuario no encontrada") },
+            { typeof(Amazon.CognitoIdentityProvider.Model.NotAuthorizedException), (HttpStatusCode.BadRequest, "Usuario o contraseña incorrectos") },
+            { typeof(Amazon.Runtime.Internal.HttpErrorResponseException), (HttpStatusCode.BadRequest, "Usuario o contraseña incorrectos") },
+            { typeof(EventNotFoundException), (HttpStatusCode.BadRequest, "Evento no encontrado") },
+            { typeof(UserEventNotFoundException), (HttpStatusCode.BadRequest, "El usuario no tiene asociado el evento seleccionado") },
+            { typeof(PatientNotFoundException), (HttpStatusCode.BadRequest, "El usuario todavía no fue dado de alta como paciente") },
+            { typeof(EventNotRelatedWithPatientException), (HttpStatusCode.BadRequest, "El evento no se encuentra relacionado al paciente") },
+            { typeof(PhysicalEventNotMatchException), (HttpStatusCode.BadRequest, "La actividad física seleccionada es errónea") },
+            { typeof(MismatchUserPatientException), (HttpStatusCode.BadRequest, "El usuario no posee el evento asociado") },
+            { typeof(UserNotFoundOnDBException), (HttpStatusCode.BadRequest, "El usuario no se encuentra registrado") },
+            { typeof(PatientInsulinRelationNotFoundException), (HttpStatusCode.BadRequest, "El paciente no se encuentra relacionado a esta insulina") },
+            { typeof(GlucoseEventNotMatchException), (HttpStatusCode.BadRequest, "La carga de glucosa seleccionada es errónea") },
+            { typeof(InsulinEventNotMatchException), (HttpStatusCode.BadRequest, "La carga de insulina seleccionada es errónea") },
+            { typeof(FoodEventNotMatchException), (HttpStatusCode.BadRequest, "El registro de comida seleccionado es erróneo") },
+            { typeof(IngredientFoodRelationNotFoundException), (HttpStatusCode.BadRequest, "Este ingrediente no se encuentra relacionado con ningún evento de comida") },
+            { typeof(CantCreatObjectS3Async), (HttpStatusCode.BadRequest, "No se pudo guardar su archivo PDF correctamente") },
+            { typeof(ExaminationEventNotFoundException), (HttpStatusCode.BadRequest, "Este evento no se encuentra relacionado con ningún estudio médico") },
+            { typeof(CantDeleteObjectS3Async), (HttpStatusCode.BadRequest, "No se pudo eliminar su archivo PDF correctamente") }
+        };
+
+            if (exceptionMap.TryGetValue(ex.GetType(), out var details)) // details va a guardar el código de estado + el mensaje (si es que existe)
+            {
+                return details;
+            }
+            return (HttpStatusCode.InternalServerError, "Ocurrió un error inesperado");
+        }
+
     } 
 }
