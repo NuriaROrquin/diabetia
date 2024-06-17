dcd19f0b94140d2ef509f17c35f48d014791df82
NROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2024-06-17 17:27:58 -0300

    Try env variables

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/auth/login",
-                permanent: true,
-            },
-            {
-                source: "/auth",
-                destination: "/auth/login",
-                permanent: true,
-            },
-        ];
-    },
 };
 
 export default nextConfig;
