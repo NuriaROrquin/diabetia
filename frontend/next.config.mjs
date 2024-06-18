c58f4bf3435b707a9847eceb9fdb3cfe39c167b2
NROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2024-06-18 00:04:58 -0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/auth/login",
+                permanent: true,
+            },
+            {
+                source: "/auth",
+                destination: "/auth/login",
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
