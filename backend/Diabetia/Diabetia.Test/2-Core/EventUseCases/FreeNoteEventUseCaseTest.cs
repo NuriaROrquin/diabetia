3ca3e8ce6e1891888a83811f352408b3e896fb17
Pablo Antunez <pablooantunez@hotmail.com>
2024-06-20 00:58:40 -0300

    Add EditFreeNote (useCase + repository)

--- backend/Diabetia/Diabetia.Test/2-Core/EventUseCases/FreeNoteEventUseCaseTest.cs
+++ backend/Diabetia/Diabetia.Test/2-Core/EventUseCases/FreeNoteEventUseCaseTest.cs
@@ -1,10 +1,12 @@
 ﻿using Diabetia.Application.UseCases.EventUseCases;
+using Diabetia.Domain.Entities.Events;
 using Diabetia.Domain.Exceptions;
 using Diabetia.Domain.Models;
 using Diabetia.Domain.Repositories;
 using Diabetia.Domain.Services;
 using Diabetia.Interfaces;
 using FakeItEasy;
+using Microsoft.Extensions.Logging;
 
 namespace Diabetia.Test._2_Core.EventUseCases
 {
@@ -13,15 +15,16 @@ namespace Diabetia.Test._2_Core.EventUseCases
         private readonly IEventRepository _fakeEventRepository;
         private readonly IPatientValidator _fakePatientValidator;
         private readonly IUserRepository _fakeUserRepository;
+        private readonly IPatientEventValidator _fakePatientEventValidator;
         private readonly FreeNoteUseCase _fakeFreeNoteUseCase;
 
         public FreeNoteEventUseCaseTest()
         {
             _fakeEventRepository = A.Fake<IEventRepository>();
             _fakePatientValidator = A.Fake<IPatientValidator>();
-            //_fakePatientEventValidator = A.Fake<IPatientEventValidator>();
+            _fakePatientEventValidator = A.Fake<IPatientEventValidator>();
             _fakeUserRepository = A.Fake<IUserRepository>();
-            _fakeFreeNoteUseCase = new FreeNoteUseCase(_fakeEventRepository, _fakePatientValidator, _fakeUserRepository);
+            _fakeFreeNoteUseCase = new FreeNoteUseCase(_fakeEventRepository, _fakePatientValidator, _fakeUserRepository, _fakePatientEventValidator);
         }
 
         // --------------------------------------- ⬇⬇ Add FreeNote Event ⬇⬇ ---------------------------------------
@@ -60,6 +63,82 @@ namespace Diabetia.Test._2_Core.EventUseCases
             // Act & Assert
             await Assert.ThrowsAsync<PatientNotFoundException>(() => _fakeFreeNoteUseCase.AddFreeNoteEventAsync(email, freeNoteEvent));
             A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
+        }
+
+        // --------------------------------------- ⬇⬇ Edit FreeNote Event ⬇⬇ ---------------------------------------
+
+        [Fact]
+        public async Task EditPhysicalActivityUseCase_WhenCalledWithValidData_ShouldUpdateEventSuccessfully()
+        {
+            // Assert
+            var email = "emailTest@example.com";
+            var freeNoteEvent = new CargaEvento()
+            {
+                Id = 1,
+                FechaEvento = DateTime.Now.AddDays(1),
+                NotaLibre = "Test Note",
+                FechaActual = DateTime.Now,
+                FueRealizado = false,
+                EsNotaLibre = true
+            };
+
+            A.CallTo(() => _fakeEventRepository.GetEventByIdAsync(freeNoteEvent.Id)).Returns(freeNoteEvent);
+
+            // Act
+            await _fakeFreeNoteUseCase.EditFreeNoteEventAsync(email, freeNoteEvent);
+
+            // Assert 
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakeEventRepository.GetEventByIdAsync(freeNoteEvent.Id)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakePatientEventValidator.ValidatePatientEvent(email, freeNoteEvent)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakeEventRepository.EditFreeNoteEventAsync(freeNoteEvent)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task EditPhysicalActivityUseCase_WhenCalledWithInvalidPatient_ThrowsPatientNotFoundException()
+        {
+            // Assert
+            var email = "emailTest@example.com";
+            var freeNoteEvent = new CargaEvento()
+            {
+                Id = 1,
+                FechaEvento = DateTime.Now.AddDays(1),
+                NotaLibre = "Test Note",
+                FechaActual = DateTime.Now,
+                FueRealizado = false,
+                EsNotaLibre = true
+            };
+
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).Throws<PatientNotFoundException>();
+
+            // Act & Assert 
+            await Assert.ThrowsAsync<PatientNotFoundException>(() => _fakeFreeNoteUseCase.EditFreeNoteEventAsync(email, freeNoteEvent));
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task EditPhysicalActivityUseCase_WhenCalledWithInvalidEvent_ThrowsEventNotRelatedWithPatientException()
+        {
+            // Assert
+            var email = "emailTest@example.com";
+            var freeNoteEvent = new CargaEvento()
+            {
+                Id = 1,
+                FechaEvento = DateTime.Now.AddDays(1),
+                NotaLibre = "Test Note",
+                FechaActual = DateTime.Now,
+                FueRealizado = false,
+                EsNotaLibre = true
+            };
+
+            A.CallTo(() => _fakeEventRepository.GetEventByIdAsync(freeNoteEvent.Id)).Returns(freeNoteEvent);
+            A.CallTo(() => _fakePatientEventValidator.ValidatePatientEvent(email, freeNoteEvent)).Throws<EventNotRelatedWithPatientException>();
+
+            // Act & Assert 
+            await Assert.ThrowsAsync<EventNotRelatedWithPatientException>(() => _fakeFreeNoteUseCase.EditFreeNoteEventAsync(email, freeNoteEvent));
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakeEventRepository.GetEventByIdAsync(freeNoteEvent.Id)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakePatientEventValidator.ValidatePatientEvent(email, freeNoteEvent)).MustHaveHappenedOnceExactly();
         }
     }
 }
--- backend/Diabetia/Diabetia.Test/3-Infraestructure/Repositories/EventTests/FreeNoteTest.cs
+++ backend/Diabetia/Diabetia.Test/3-Infraestructure/Repositories/EventTests/FreeNoteTest.cs
@@ -46,5 +46,42 @@ namespace Diabetia.Test._3_Infraestructure.Repositories.EventTests
             return mockContext;
         }
 
+        // --------------------------------------- ⬇⬇ EditFreeNoteEvent Test ⬇⬇ ---------------------------------------
+        [Fact]
+        public async Task EditFreeNoteEvent_GivenValidData_ShouldUpdateEventSuccessfully()
+        {
+            // Arrange
+            var mockContext = CreateMockContextEditPassCorrect();
+
+            var fakeRepository = new EventRepository(mockContext.Object);
+
+            var freeNoteEvent = new CargaEvento()
+            {
+                Id = 1,
+                NotaLibre = "Test",
+                EsNotaLibre = true,
+                FechaEvento = DateTime.Now.AddDays(1),
+                FueRealizado = false
+            };
+
+            // Act
+            await fakeRepository.EditFreeNoteEventAsync(freeNoteEvent);
+
+            // Assert
+            mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+
+            var @event = mockContext.Object.CargaEventos.First();
+            Assert.Equal("Test", freeNoteEvent.NotaLibre);
+        }
+        private Mock<diabetiaContext> CreateMockContextEditPassCorrect()
+        {
+            var @event = new CargaEvento { Id = 1, IdPaciente = 1, FechaEvento = DateTime.Now, NotaLibre = "Test Note", EsNotaLibre = true };
+
+            var mockContext = new Mock<diabetiaContext>();
+
+            mockContext.Setup(m => m.CargaEventos).ReturnsDbSet(new List<CargaEvento> { @event });
+
+            return mockContext;
+        }
     }
 }
