a2f7c4310573a8a8f2b44658ed06a88c392b13db
sanopera <91997129+sanopera@users.noreply.github.com>
2024-06-20 13:08:25 -0300

    commit

--- backend/Diabetia/Diabetia.API/Controllers/Profile/ProfileController.cs
+++ backend/Diabetia/Diabetia.API/Controllers/Profile/ProfileController.cs
@@ -2,6 +2,7 @@ using Diabetia.Application.UseCases;
 using Diabetia.Domain.Entities;
 using Diabetia.Domain.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 using Microsoft.IdentityModel.Tokens;
@@ -16,40 +17,45 @@ namespace Diabetia.API.Controllers.Profile
     public class ProfileController : ControllerBase
     {
         private readonly ILogger<ProfileController> _logger;
-
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly DataUserUseCase _dataUserUseCase;
 
-        public ProfileController(ILogger<ProfileController> logger, DataUserUseCase dataUserUseCase)
+        public ProfileController(ILogger<ProfileController> logger, DataUserUseCase dataUserUseCase, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
             _dataUserUseCase = dataUserUseCase;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         [HttpGet("getUserInfo")]
         [Authorize]
-        public async Task<User> GetEditUserInfo([FromQuery] string email)
+        public async Task<User> GetEditUserInfo()
         {
+            var email = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Email)?.Value;
             return await _dataUserUseCase.GetEditUserInfo(email);
         }
 
         [HttpGet("getPatientInfo")]
         [Authorize]
-        public async Task<Patient> GetPatientInfo([FromQuery] string email)
+        public async Task<Patient> GetPatientInfo()
         {
+            var email = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Email)?.Value;
             return await _dataUserUseCase.GetPatientInfo(email);
         }
 
         [HttpGet("getExercisePatientInfo")]
         [Authorize]
-        public async Task<Exercise_Patient> GetExerciseInfo([FromQuery] string email)
+        public async Task<Exercise_Patient> GetExerciseInfo()
         {
+            var email = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Email)?.Value;
             return await _dataUserUseCase.GetExerciseInfo(email);
         }
 
         [HttpGet("getPatientDeviceInfo")]
         [Authorize]
-        public async Task<Device_Patient> GetPatientDeviceInfo([FromQuery] string email)
+        public async Task<Device_Patient> GetPatientDeviceInfo()
         {
+            var email = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Email)?.Value;
             return await _dataUserUseCase.GetPatientDeviceInfo(email);
         }
 
