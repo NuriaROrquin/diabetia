25a8abcd73e96a3c01451977e773f95b1c21a9d3
Pablo Antunez <pablooantunez@hotmail.com>
2024-06-27 16:48:38 -0300

    Update InsulinResponse.cs

--- backend/Diabetia/Diabetia.API/DTO/ReportingResponse/InsulinResponse.cs
+++ backend/Diabetia/Diabetia.API/DTO/ReportingResponse/InsulinResponse.cs
@@ -5,14 +5,16 @@ namespace Diabetia.API.DTO.ReportingResponse
     public class InsulinResponse
     {
         public int Value { get; set; }
-        public string Name { get; set; }
+        public string Time { get; set; }
 
         public static InsulinResponse FromInsulinEvent(EventoInsulina evento)
         {
+            DateTime Date = evento.IdCargaEventoNavigation.FechaEvento;
+            string stringDate = Date.ToString("dd/MM/yyyy");
             return new InsulinResponse
             {
                 Value = evento.InsulinaInyectada.Value, 
-                Name = ""
+                Time = stringDate
             };
         }
     }
