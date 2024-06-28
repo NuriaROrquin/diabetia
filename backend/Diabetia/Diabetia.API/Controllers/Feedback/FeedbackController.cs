b198a1f2e71b20433cf53ee580d81061a6d03940
Pablo Antunez <pablooantunez@hotmail.com>
2024-06-28 20:29:49 -0300

    Creation Controller & UseCase

---
+++ backend/Diabetia/Diabetia.API/Controllers/Feedback/FeedbackController.cs
@@ -0,0 +1,26 @@
+﻿using Diabetia.Application.UseCases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diabetia.API.Controllers.Feedback
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly FeedbackUseCase _feedbackUseCase;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public FeedbackController(FeedbackUseCase feedbackUseCase, IHttpContextAccessor httpContextAccessor)
+        {
+            _feedbackUseCase = feedbackUseCase;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ backend/Diabetia/Diabetia.Application/UseCases/FeedbackUseCase.cs
@@ -0,0 +1,14 @@
+﻿using Diabetia.Domain.Utilities.Validations;
+
+namespace Diabetia.Application.UseCases
+{
+    public class FeedbackUseCase
+    {
+        private readonly PatientValidator _patientValidator;
+
+        public FeedbackUseCase(PatientValidator patientValidator)
+        {
+            _patientValidator = patientValidator;
+        }
+    }
+}
