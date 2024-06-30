9b75150d3922c3e334310ffbb0f075cbd661de75
sanopera <91997129+sanopera@users.noreply.github.com>
2024-06-30 03:20:17 -0300

    commit

--- backend/Diabetia/Diabetia.Test/2-Core/ReportingUseCases/PhysicalActivityUseCaseTest.cs
+++ backend/Diabetia/Diabetia.Test/2-Core/ReportingUseCases/PhysicalActivityUseCaseTest.cs
@@ -1,12 +1,12 @@
 ﻿using Diabetia.Application.UseCases.ReportingUseCases;
-using Diabetia.Domain.Entities;
 using Diabetia.Domain.Entities.Reporting;
+using Diabetia.Domain.Exceptions;
 using Diabetia.Domain.Models;
 using Diabetia.Domain.Repositories;
 using Diabetia.Domain.Services;
 using Diabetia.Interfaces;
 using FakeItEasy;
-using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System.Security.AccessControl;
 
 namespace Diabetia_Core.Reporting
 {
@@ -17,7 +17,7 @@ namespace Diabetia_Core.Reporting
         private readonly IReportingRepository _fakeReportingRepository;
         private readonly PhysicalActivityReportUseCase _fakePhysicalActivityReportUseCase;
 
-        public PhysicalActivityUseCaseTest() 
+        public PhysicalActivityUseCaseTest()
         {
             _fakePatientValidator = A.Fake<IPatientValidator>();
             _fakeUserRepository = A.Fake<IUserRepository>();
@@ -25,9 +25,79 @@ namespace Diabetia_Core.Reporting
             _fakePhysicalActivityReportUseCase = new PhysicalActivityReportUseCase(_fakePatientValidator, _fakeUserRepository, _fakeReportingRepository);
         }
 
+
         [Fact]
-        
+
         public async Task GetPhysicalActivityToReporting_GivingValidData_ShouldGetPhysicalActivitySuccessfully()
+        {
+            var email = "test@diabetia.com";
+            var dateFrom = DateTime.Now.AddDays(1);
+            var dateTo = DateTime.Now.AddDays(3);
+            var patient = new Paciente() { Id = 1 };
+            var physicalActivities = new List<EventSummary>
+            {
+                new EventSummary
+                {
+                     EventDay = DateTime.Now.AddDays(2),
+                      AmountEvents = 1
+                }
+            };
+
+            A.CallTo(() => _fakeUserRepository.GetPatient(email)).Returns(patient);
+            A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventSummaryByPatientId(patient.Id, dateFrom, dateTo)).Returns(Task.FromResult(physicalActivities));
+
+            //Act
+            var response = await _fakePhysicalActivityReportUseCase.GetPhysicalActivityToReporting(email, dateFrom, dateTo);
+
+            //Assert
+            A.CallTo(() => _fakeUserRepository.GetPatient(email)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventSummaryByPatientId(patient.Id, dateFrom, dateTo)).MustHaveHappenedOnceExactly();
+
+
+        }
+
+        [Fact]
+
+        public async Task GetPhysicalActivityToReporting_GivingValidData_ShouldGetEmptyPhysicalActivitySuccessfully()
+        {
+            var email = "test@diabetia.com";
+            var dateFrom = DateTime.Now.AddDays(1);
+            var dateTo = DateTime.Now.AddDays(3);
+            var patient = new Paciente() { Id = 1 };
+            var physicalActivities = new List<EventSummary>();
+
+            A.CallTo(() => _fakeUserRepository.GetPatient(email)).Returns(patient);
+            A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventSummaryByPatientId(patient.Id, dateFrom, dateTo)).Returns(Task.FromResult(physicalActivities));
+
+            //Act
+            var response = await _fakePhysicalActivityReportUseCase.GetPhysicalActivityToReporting(email, dateFrom, dateTo);
+
+            //Assert
+            A.CallTo(() => _fakeUserRepository.GetPatient(email)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventSummaryByPatientId(patient.Id, dateFrom, dateTo)).MustHaveHappenedOnceExactly();
+            Assert.Empty(response);
+        }
+
+        [Fact]
+
+        public async Task GetPhysicalActivityToReporting_GivingInvalidPatient_ThrowPatientNotFoundException()
+        {
+            var email = "test@diabetia.com";
+            var dateFrom = DateTime.Now.AddDays(1);
+            var dateTo = DateTime.Now.AddDays(3);
+
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).Throws<PatientNotFoundException>();
+
+            //ActAndAssert
+            await Assert.ThrowsAsync<PatientNotFoundException>(() => _fakePhysicalActivityReportUseCase.GetPhysicalActivityToReporting(email, dateFrom, dateTo));
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+
+        public async Task GetPhysicalActivityDurationToReporting_GivingValidData_ShouldGetPhysicalActivityDurationSuccessfully()
         {
             var email = "test@diabetia.com";
             var dateFrom = DateTime.Now.AddDays(1);
@@ -40,19 +110,67 @@ namespace Diabetia_Core.Reporting
                     ActivityName = "Running",
                     TotalDuration = 30
                 }
-            };
+             };
 
             A.CallTo(() => _fakeUserRepository.GetPatient(email)).Returns(patient);
             A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventDurationsByPatientId(patient.Id, dateFrom, dateTo)).Returns(Task.FromResult(physicalActivitiesDurations));
 
-            //Act
-            var response = _fakePhysicalActivityReportUseCase.GetPhysicalActivityToReporting(email, dateFrom, dateTo);
+            // Act
+            var response = await _fakePhysicalActivityReportUseCase.GetPhysicalActivityDurationToReporting(email, dateFrom, dateTo);
 
-            //Assert
+            // Assert
             A.CallTo(() => _fakeUserRepository.GetPatient(email)).MustHaveHappenedOnceExactly();
             A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
-            A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventSummaryByPatientId(patient.Id, dateFrom, dateTo)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventDurationsByPatientId(patient.Id, dateFrom, dateTo)).MustHaveHappenedOnceExactly();
 
+            // Assert the response
+            Assert.NotNull(response);
+            Assert.Single(response); // Verifies that there is exactly one item in the response
+
+            var actualActivity = response.First();
+            Assert.Equal("Running", actualActivity.ActivityName);
+            Assert.Equal(30, actualActivity.TotalDuration);
+
+        }
+
+        [Fact]
+
+        public async Task GetPhysicalActivityDurationToReporting_GivingValidData_ShouldGetEmptyPhysicalActivityDurationSuccessfully()
+        {
+            var email = "test@diabetia.com";
+            var dateFrom = DateTime.Now.AddDays(1);
+            var dateTo = DateTime.Now.AddDays(3);
+            var patient = new Paciente() { Id = 1 };
+            var physicalActivitiesDurations = new List<ActivityDurationSummary>();
+
+            A.CallTo(() => _fakeUserRepository.GetPatient(email)).Returns(patient);
+            A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventDurationsByPatientId(patient.Id, dateFrom, dateTo)).Returns(Task.FromResult(physicalActivitiesDurations));
+
+            // Act
+            var response = await _fakePhysicalActivityReportUseCase.GetPhysicalActivityDurationToReporting(email, dateFrom, dateTo);
+
+            // Assert
+            A.CallTo(() => _fakeUserRepository.GetPatient(email)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakeReportingRepository.GetPhysicalActivityEventDurationsByPatientId(patient.Id, dateFrom, dateTo)).MustHaveHappenedOnceExactly();
+
+            Assert.Empty(response);
+
+        }
+
+        [Fact]
+
+        public async Task GetPhysicalActivityDurationToReporting_GivingInvalidPatient_ThrowPatientNotFoundException()
+        {
+            var email = "test@diabetia.com";
+            var dateFrom = DateTime.Now.AddDays(1);
+            var dateTo = DateTime.Now.AddDays(3);
+
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).Throws<PatientNotFoundException>();
+
+            //ActAndAssert
+            await Assert.ThrowsAsync<PatientNotFoundException>(() => _fakePhysicalActivityReportUseCase.GetPhysicalActivityDurationToReporting(email, dateFrom, dateTo));
+            A.CallTo(() => _fakePatientValidator.ValidatePatient(email)).MustHaveHappenedOnceExactly();
         }
     }
 }
